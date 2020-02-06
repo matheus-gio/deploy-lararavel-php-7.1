bfc7145c7cbffd9126fdfb0ba859d1c9d31e2747
Matheus Lucena <matheus.giovanni.lu@gmail.com>
2020-02-05 21:24:19 -0300

    teste

--- script.sh
+++ script.sh
@@ -17,3 +17,11 @@ mv composer.phar /usr/bin/composer
 
 
 cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+
+iptables -F
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -j DROP
